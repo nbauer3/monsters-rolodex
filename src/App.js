aa87edf62eea7df0b5708964603c425db1069304
Nick <Bauer_Nicholas@bah.com>
2019-10-25 12:36:02 -0400

    Added Components, Reconfigured Architecture

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
 import React, {Component} from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import {CardList} from './components/card-list/card-list.component'
 
 class App extends Component {
   constructor() {
     super();
-
     this.state = {
-      string: 'Hello Nick Bauer',
+      monsters: []
     }
   }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then(response => response.json())
+      .then(users => this.setState({monsters: users}))
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>{ this.state.string }</p>
-          <button onClick={() => this.setState({ string: 'Hello Nick 2' })}>
-            Change Text
-          </button>
-        </header>
+        <CardList monsters={this.state.monsters}/>
       </div>
     );
   }
