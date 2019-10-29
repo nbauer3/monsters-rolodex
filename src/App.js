2067bcf13cde8cb7b6aae48ed30ff99610d3bd86
Nick <Bauer_Nicholas@bah.com>
2019-10-28 21:14:26 -0400

    App Development Completed, Deployed on github.io

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
-//import logo from './logo.svg';
 import './App.css';
 import {CardList} from './components/card-list/card-list.component'
+import {SearchBox} from './components/search-box/search-box.component'
 
 class App extends Component {
   constructor() {
@@ -18,6 +18,10 @@ class App extends Component {
       .then(users => this.setState({monsters: users}))
   }
 
+  handleChange = e => {
+    this.setState({ searchField: e.target.value })
+  }
+
   render() {
     //destructing
     const  { monsters, searchField } = this.state;
@@ -27,13 +31,11 @@ class App extends Component {
 
     return (
       <div className="App">
-        <input 
-          type='search' 
-          placeholder='Search Monsters' 
-          onChange={ e => {  
-            this.setState({ searchField: e.target.value }); 
-            console.log(e.target.value)
-            }}/>
+        <h1>Monsters Rolodex</h1>
+        <SearchBox
+          placeholder='Filter Monsters'
+          handleChange={ this.handleChange }
+        />
         <CardList monsters={filteredMonsters}/>
       </div>
     );
